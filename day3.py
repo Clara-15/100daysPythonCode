f3507c1d75474e13b43681047243266519572346
Your Name <you@example.com>
2024-06-26 16:53:28 +0300

    Day 3 of python coding

---
+++ day3.py
@@ -0,0 +1,19 @@
+#Concatenation
+
+# Taking inputs
+nameOfFood = input("What's today menu?")
+typeOfPlant = input("Enter the type of plant: ")
+methodOfCooking = input("Enter the method of cooking: ")
+describeFood = input("Enter a description of the food: ")
+diyItem = input("Enter a DIY item: ")
+
+# Printing the menu description
+print("What's on the menu today?", nameOfFood, "with", typeOfPlant, describeFood, "ugaserved on", diyItem)
+
+
+
+
+
+
+
+
