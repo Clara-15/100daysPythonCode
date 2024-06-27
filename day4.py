0174907a0d8b20128b66066ca02507225dc750ac
Your Name <you@example.com>
2024-06-27 19:16:09 +0300

    day 4 of python coding

---
+++ day4.py
@@ -0,0 +1,15 @@
+#creating a story and coloring it
+name=input("What's the name of your baby name?")
+animal=input("What's your baby's favourite bird?")
+
+print("\033[34mBedtime adventure story for your baby\033[0m")
+print("\033[35mOnce upon a time\033[0m, in a cozy room, baby",  name, "loved adventure stories about faraway lands.")
+print("One night, \033[32ma gentle breeze whispered\033[0m", name, "let's go on a\033[35m magical adventure.\033[0m")
+print("A \033[31msparkling path appeared,\033[0m leading", name, "into a glowing forest.")
+print("The", animal,  "said, follow me to the enchanted lake.")
+print("At the lake, \033[32mfairies danced on shimmering waters.\033[0m 'Help us find our lost treasure,' they chimed.") 
+print("Together, Alex and \033[33mthe fairies journeyed through a magical forest,\033[0m, finding a hidden chest filled with gems and toys.")
+print("The fairies cheered, \033[31mYou're our hero, Alex!\033[0m They gave", name, "a magical star as thanks.")
+print("Guided back by the", animal, "Alex cuddled the star, \033[33mdreaming of more adventures\033[0m before drifting into a peaceful sleep.")
+print("\033[34mGoodnight\033[0m", name, "\033[34mSweet dreams💤\033[0m")
+ 
